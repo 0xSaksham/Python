64c51fc23fbafdf7f73131ea27a4377df3efbbb7
Saksham Gupta <sakshamgupta.ai@gmail.com>
2023-08-03 18:38:26 +0530

    A lot of Practice

---
+++ bigger_table.py
@@ -0,0 +1,20 @@
+guests = ['Elon Musk',
+          'Andrew Tate',
+          'Jordan Peterson',
+          ]
+
+
+
+guests.insert(0,' Shri Narendra Modi Ji')
+
+guests.insert(2,' Shri Manoj Kumar Gupta')
+
+guests.append('Saksham Gupta')
+
+print("I would love to invite: " + guests[0] + " for Dinner \n")
+print("I would love to invite: " + guests[1] + " for Dinner \n")
+print("I would love to invite: " + guests[2] + " for Dinner \n")
+print("I would love to invite: " + guests[3] + " for Dinner \n")
+print("I would love to invite: " + guests[4] + " for Dinner \n")
+print("I would love to invite: " + guests[5] + " for Dinner \n")
+
--- updated_guest_list.py
+++ updated_guest_list.py
@@ -3,6 +3,13 @@ guests = ['Elon Musk',
           'Jordan Peterson',
           ]
 
+guest_not_available = guests.pop()
+
+print ( "Unfortunately " + guest_not_available + " is not available for Dinner \n")
+
+guests.append("Shri Narendra Modi ")
+print("Instead of Him, " + guests[2] + "will be joining us for Dinner! \n")
+
 print("I would love to invite: " + guests[0] + " for Dinner \n")
 print("I would love to invite: " + guests[1] + " for Dinner \n")
 print("I would love to invite: " + guests[2] + " for Dinner \n")
