51170909297ca6d818c8c6d28f54679bb9191e64
Saksham Gupta <sakshamgupta.ai@gmail.com>
2023-08-03 17:51:44 +0530

    Python Book

---
+++ favourite_vehicles.py
@@ -0,0 +1,17 @@
+favourite_cars=['Mahindra Bolero',
+                'Mahindra Thar 4X4 Automatic Diesel ',
+                'Mahindra Scorpio Classic',
+                'Mahindra Scorpio N',
+                'Toyota Supra MK-4 6Speed Manual Transmission',
+                'Toyota Fortuner Legender Sigma4',
+                'Mahindra XUV700 AX7',
+                'BMW 3 Series 350i Petrol M-sport Edition']
+
+print('In future I would like to own this car with this specific configuration: ' + favourite_cars[0] )
+print('In future I would like to own this car with this specific configuration: ' + favourite_cars[1] )
+print('In future I would like to own this car with this specific configuration: ' + favourite_cars[2] )
+print('In future I would like to own this car with this specific configuration: ' + favourite_cars[3] )
+print('In future I would like to own this car with this specific configuration: ' + favourite_cars[4] )
+print('In future I would like to own this car with this specific configuration: ' + favourite_cars[5] )
+print('In future I would like to own this car with this specific configuration: ' + favourite_cars[6] )
+print('In future I would like to own this car with this specific configuration: ' + favourite_cars[7] )
---
+++ numbers_cases.py
@@ -0,0 +1,11 @@
+
+
+# Declaring my favourite number in int value
+favourite_number = 8
+
+
+# Printing the message with favorite number using string concatenation and converting it into a String.
+
+
+print("My Favourite Number is : "+ str(favourite_number))
+
