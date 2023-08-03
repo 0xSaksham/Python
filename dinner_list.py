bb37cd8d080754d2929752214ba21d6c8ec58a13
Saksham Gupta <sakshamgupta.ai@gmail.com>
2023-08-03 18:20:17 +0530

    Added dinner_list.py

---
+++ dinner_list.py
@@ -0,0 +1,9 @@
+guests = ['Elon Musk',
+          'Andrew Tate',
+          'Jordan Peterson',
+          ]
+
+print("I would love to invite: " + guests[0] + " for Dinner")
+print("I would love to invite: " + guests[1] + " for Dinner")
+print("I would love to invite: " + guests[2] + " for Dinner")
+
