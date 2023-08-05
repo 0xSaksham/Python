933182fa84a47de3134df0be6b5fedd48921c0e2
Saksham Gupta <sakshamgupta.ai@gmail.com>
2023-08-05 21:11:13 +0530

    Commit #3

---
+++ intentional_error.py
@@ -0,0 +1,10 @@
+list = [1,2,3,4,5]
+
+print(list[6])
+
+# python intentional_error.py                                                 
+# Traceback (most recent call last):
+#   File "C:\Users\saksh\Courses and Stuff\Code and Programs\Python\Python\intentional_error.py", line 3, in <module>
+#     print(list[6])
+#           ~~~~^^^
+# IndexError: list index out of range 
